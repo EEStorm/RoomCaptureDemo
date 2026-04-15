2c8fc5626e969fa4d547a583cb31a90cc638a7e9
shenbo027 <shenbo027@ke.com>
2026-04-15 15:37:55 +0800

    视频写入相册

--- ViewControllers/ParameterCamera/CDVideoListViewController.m
+++ ViewControllers/ParameterCamera/CDVideoListViewController.m
@@ -1,12 +1,14 @@
 #import "CDVideoListViewController.h"
 #import "CDCameraService.h"
 #import <AVKit/AVKit.h>
+#import <Photos/Photos.h>
 
 @interface CDVideoListViewController () <UITableViewDelegate, UITableViewDataSource>
 @property (nonatomic, strong) UITableView *tableView;
 @property (nonatomic, strong) NSArray *videos;
 @property (nonatomic, strong) UIView *bottomBar;
 @property (nonatomic, strong) UIButton *deleteAllButton;
+@property (nonatomic, strong) UIButton *saveAllButton;
 @end
 
 @implementation CDVideoListViewController
@@ -22,7 +24,10 @@
     CGRect tableFrame = CGRectMake(0, 0, width, height - bottomBarHeight - insets.bottom);
     self.tableView.frame = tableFrame;
     self.bottomBar.frame = CGRectMake(0, CGRectGetMaxY(tableFrame), width, bottomBarHeight + insets.bottom);
-    self.deleteAllButton.frame = CGRectMake(20, 12, self.bottomBar.bounds.size.width - 40, 46);
+
+    CGFloat buttonWidth = (self.bottomBar.bounds.size.width - 50) / 2;
+    self.saveAllButton.frame = CGRectMake(20, 12, buttonWidth, 46);
+    self.deleteAllButton.frame = CGRectMake(CGRectGetMaxX(self.saveAllButton.frame) + 10, 12, buttonWidth, 46);
 }
 
 - (void)viewDidLoad {
@@ -45,6 +50,16 @@
     self.bottomBar.backgroundColor = [UIColor secondarySystemBackgroundColor];
     [self.view addSubview:self.bottomBar];
 
+    self.saveAllButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [self.saveAllButton setTitle:@"保存所有到相册" forState:UIControlStateNormal];
+    self.saveAllButton.titleLabel.font = [UIFont boldSystemFontOfSize:16];
+    [self.saveAllButton setTitleColor:[UIColor systemBlueColor] forState:UIControlStateNormal];
+    self.saveAllButton.layer.borderColor = [UIColor systemBlueColor].CGColor;
+    self.saveAllButton.layer.borderWidth = 1.0;
+    self.saveAllButton.layer.cornerRadius = 8.0;
+    [self.saveAllButton addTarget:self action:@selector(saveAllToAlbum) forControlEvents:UIControlEventTouchUpInside];
+    [self.bottomBar addSubview:self.saveAllButton];
+
     self.deleteAllButton = [UIButton buttonWithType:UIButtonTypeSystem];
     [self.deleteAllButton setTitle:@"一键删除所有" forState:UIControlStateNormal];
     self.deleteAllButton.titleLabel.font = [UIFont boldSystemFontOfSize:16];
@@ -63,6 +78,8 @@
     [self.tableView reloadData];
     self.deleteAllButton.enabled = self.videos.count > 0;
     self.deleteAllButton.alpha = self.deleteAllButton.enabled ? 1.0 : 0.4;
+    self.saveAllButton.enabled = self.videos.count > 0;
+    self.saveAllButton.alpha = self.saveAllButton.enabled ? 1.0 : 0.4;
 }
 
 - (void)dismissList {
@@ -83,6 +100,54 @@
         [weakSelf loadVideos];
     }]];
     [self presentViewController:alert animated:YES completion:nil];
+}
+
+- (void)saveAllToAlbum {
+    if (self.videos.count == 0) {
+        return;
+    }
+    __weak typeof(self) weakSelf = self;
+    [PHPhotoLibrary requestAuthorization:^(PHAuthorizationStatus status) {
+        __strong typeof(weakSelf) strongSelf = weakSelf;
+        if (!strongSelf) {
+            return;
+        }
+        if (status != PHAuthorizationStatusAuthorized) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                __strong typeof(weakSelf) strongSelf2 = weakSelf;
+                if (!strongSelf2) return;
+                UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"无法保存"
+                                                                               message:@"请在设置中允许访问相册"
+                                                                        preferredStyle:UIAlertControllerStyleAlert];
+                [alert addAction:[UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:nil]];
+                [strongSelf2 presentViewController:alert animated:YES completion:nil];
+            });
+            return;
+        }
+        NSInteger count = strongSelf.videos.count;
+        __block NSInteger savedCount = 0;
+        for (NSURL *url in strongSelf.videos) {
+            NSString *path = url.path;
+            [[PHPhotoLibrary sharedPhotoLibrary] performChanges:^{
+                [PHAssetChangeRequest creationRequestForAssetFromVideoAtFileURL:[NSURL fileURLWithPath:path]];
+            } completionHandler:^(BOOL success, NSError *error) {
+                __strong typeof(weakSelf) strongSelf3 = weakSelf;
+                if (!strongSelf3) return;
+                savedCount++;
+                if (savedCount == count) {
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        __strong typeof(weakSelf) strongSelf4 = weakSelf;
+                        if (!strongSelf4) return;
+                        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"保存完成"
+                                                                                       message:[NSString stringWithFormat:@"已保存 %ld 个视频到相册", (long)count]
+                                                                                preferredStyle:UIAlertControllerStyleAlert];
+                        [alert addAction:[UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:nil]];
+                        [strongSelf4 presentViewController:alert animated:YES completion:nil];
+                    });
+                }
+            }];
+        }
+    }];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
